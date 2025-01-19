1399305ab136e7309dcb52b9e20560d301edc9a8
Tusharprabhu <tusharprabhu2004@gmail.com>
2025-01-19 15:11:40 +0530

    Refactor Printcheque component to use dropdowns for name and bank selection, and implement print functionality. Add Report component to display data in a table format with totals.

--- my-react-app/src/pages/Printcheque.jsx
+++ my-react-app/src/pages/Printcheque.jsx
@@ -5,9 +5,9 @@ import icici from "../images/icici.jpeg";
 const Printcheque = () => {
   const [item, setItem] = useState(null);
   const [selectedName, setSelectedName] = useState("");
-  const [selectedDescription, setSelectedDescription] = useState("");
-  const [isNameDropdownOpen, setIsNameDropdownOpen] = useState(false);
-  const [isDescriptionDropdownOpen, setIsDescriptionDropdownOpen] = useState(false);
+  const [selectedbank, setselectedbank] = useState("");
+  const [printedName, setPrintedName] = useState("");
+  const [Printedbank, setPrintedbank] = useState("");
 
   useEffect(() => {
     axios
@@ -20,111 +20,98 @@ const Printcheque = () => {
       });
   }, []);
 
+  const handlePrint = () => {
+    setPrintedName(selectedName);
+    setPrintedbank(selectedbank);
+  };
+
   if (!item) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div className="flex flex-col justify-betwen h-full overflow-hidden">
-      {/* Top Content */}
-      <div className="p-5">
-        <h1 className="text-3xl font-bold leading-tight mb-5">Print Cheque</h1>
-        <div className="flex flex-row gap-5 mt-5">
-          {/* Dropdown for Name */}
+    <div className="flex flex-col h-full">
+      <div className="">
+        <h1 className="text-3xl font-semibold m-6">Print Cheque</h1>
+        <div className="flex flex-row justify-start items-center gap-8 m-5 p-4">
           <div className="relative">
-            <button
-              onClick={() => setIsNameDropdownOpen((prev) => !prev)}
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex items-center"
-              type="button"
+            <input
+              type="text"
+              placeholder="Enter Name"
+              value={selectedName}
+              onChange={(e) => setSelectedName(e.target.value)}
+              className="w-full text-gray-800 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-blue-500 font-medium rounded-lg text-sm px-4 py-2 shadow-md border border-gray-300 transition-all"
+            />
+            <select
+              value={selectedName}
+              onChange={(e) => setSelectedName(e.target.value)}
+              className="w-full text-gray-800 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-blue-500 font-medium rounded-lg text-sm px-4 py-2 shadow-md border border-gray-300 transition-all mt-2"
             >
-              {selectedName || "Select Name"}
-              <svg
-                className="w-2.5 h-2.5 ml-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 10 6"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m1 1 4 4 4-4"
-                />
-              </svg>
-            </button>
-            {isNameDropdownOpen && (
-              <div className="z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 absolute mt-1">
-                <ul className="py-2 text-sm text-gray-700">
-                  {item.map((user, index) => (
-                    <li key={index}>
-                      <button
-                        onClick={() => {
-                          setSelectedName(user.name);
-                          setIsNameDropdownOpen(false);
-                        }}
-                        className="block w-full text-left px-4 py-2 hover:bg-gray-100 hover:text-gray-900"
-                      >
-                        {user.name}
-                      </button>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            )}
+              <option value="" disabled>
+                Select Name
+              </option>
+              {item.map((user, index) => (
+                <option key={index} value={user.name}>
+                  {user.name}
+                </option>
+              ))}
+            </select>
           </div>
 
-          {/* Dropdown for Description */}
           <div className="relative">
-            <button
-              onClick={() => setIsDescriptionDropdownOpen((prev) => !prev)}
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex items-center"
-              type="button"
+            <select
+              value={selectedbank}
+              onChange={(e) => setselectedbank(e.target.value)}
+              className="w-full text-gray-800 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-blue-500 font-medium rounded-lg text-sm px-4 py-2 shadow-md border border-gray-300 transition-all"
             >
-              {selectedDescription || "Select Description"}
-              <svg
-                className="w-2.5 h-2.5 ml-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 10 6"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m1 1 4 4 4-4"
-                />
-              </svg>
-            </button>
-            {isDescriptionDropdownOpen && (
-              <div className="z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 absolute mt-1">
-                <ul className="py-2 text-sm text-gray-700">
-                  {item.map((user, index) => (
-                    <li key={index}>
-                      <button
-                        onClick={() => {
-                          setSelectedDescription(user.description);
-                          setIsDescriptionDropdownOpen(false);
-                        }}
-                        className="block w-full text-left px-4 py-2 hover:bg-gray-100 hover:text-gray-900"
-                      >
-                        {user.description}
-                      </button>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            )}
+              <option value="" disabled>
+                Select bank
+              </option>
+              {item.map((user, index) => (
+                <option key={index} value={user.bank}>
+                  {user.bank}
+                </option>
+              ))}
+            </select>
           </div>
         </div>
       </div>
 
-      {/* Bottom Content: Image */}
-      <div className="flex justify-center items-end">
-        <img src={icici} alt="Cheque" className="rounded-lg shadow-lg mb-5" />
+      <button
+        onClick={handlePrint}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-52"
+      >
+        Preview Cheque
+      </button>
+
+      <button
+        onClick={() => window.print()}
+        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4 w-52"
+      >
+        Print Page
+      </button>
+
+      {/* Image at Bottom */}
+      <div id="image" className="flex justify-center m-20 items-center relative">
+        <img
+          src={icici}
+          alt="Cheque"
+          className="rounded-lg shadow-lg h-[612px] w-[792px]"
+        />
+        <div className="absolute font-bold flex flex-col justify-start top-0 left-0 text-[20px]">
+          <div className="tracking-[8px] translate-x-[34.3rem] translate-y-[17px]">
+            <p>{printedName}</p>
+          </div>
+          <div className="translate-x-[5.3rem] translate-y-[19px]">
+            <p>{printedName}</p>
+          </div>
+          <div className="translate-x-[7.5rem] translate-y-[23px]">
+            <p>{Printedbank}</p>
+          </div>
+          <div className="translate-x-[34.5rem] translate-y-[25px]">
+            <p>{Printedbank}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- my-react-app/src/pages/Report.jsx
+++ my-react-app/src/pages/Report.jsx
@@ -1,11 +1,65 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Report = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
-}
+  const [data, setData] = useState([]);
 
-export default Report
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/")
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error!", error);
+      });
+  }, []);
+
+  return (
+    <div className="relative overflow-x-auto">
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="px-6 py-3 rounded-s-lg">Name</th>
+            <th scope="col" className="px-6 py-3">IN/OUT</th>
+            <th scope="col" className="px-6 py-3">Amount</th>
+            <th scope='col' className='px-6 py-3'>TotalAmount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index} className="bg-white dark:bg-gray-800">
+              <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <Link to="/namereport" className="text-blue-500 hover:underline">
+                  {item.name}
+                </Link>
+              </th>
+              <td className={`px-6 py-4 font-bold ${item.inout.trim().toUpperCase() === 'IN' ? 'text-green-500' : 'text-red-500'}`}>
+                {item.inout}
+              </td>
+              <td className="px-6 py-4 font-bold">{item.amount}</td>
+              <td className="px-6 py-4 font-bold">
+                {item.inout.trim().toUpperCase() === 'IN' ? item.amount : -item.amount}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr className="font-semibold text-gray-900 dark:text-white">
+            <th scope="row" className="px-6 py-3 text-base">Total</th>
+            <td className="px-6 py-3 ">IN:{data.filter(item => item.inout.trim().toUpperCase() === 'IN').length} / OUT:{data.filter(item => item.inout.trim().toUpperCase() === 'OUT').length}</td>
+            <td className="px-6 py-3">{data.reduce((acc, item) => acc + item.amount, 0)}</td>
+            <td className="px-6 py-3">
+              {data.reduce((acc, item) => {
+                return item.inout.trim().toUpperCase() === 'IN' ? acc + item.amount : acc - item.amount;
+              }, 0)}
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>  
+  );
+};
+
+export default Report;
